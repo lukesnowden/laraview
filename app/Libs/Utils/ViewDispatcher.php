fad458dab8e265178e3a6def27855b5a201f98da
Luke Snowden <mail@lukesnowden.co.uk>
2018-10-05 16:28:03 +0100

    added view dispatcher

---
+++ app/Libs/Utils/ViewDispatcher.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace Laraview\Libs\Utils;
+
+use Illuminate\View\View;
+
+class ViewDispatcher
+{
+
+    /**
+     * @var string
+     */
+    private $name = '';
+
+    /**
+     * @var array
+     */
+    private $data = [];
+
+    /**
+     * ViewDispatcher constructor.
+     * @param View $view
+     */
+    public function __construct( View $view )
+    {
+        $this->data = $view->getData();
+        $this->name = $view->getName();
+        $this->dispatchEvent();
+    }
+
+    /**
+     * @param $key
+     * @param $value
+     * @return $this
+     */
+    public function attach( $key, $value )
+    {
+        $this->data[ $key ] = $value;
+        return $this;
+    }
+
+    /**
+     * @param $key
+     */
+    public function detach( $key )
+    {
+        if( isset( $this->data[ $key ] ) ) {
+            unset( $this->data[ $key ] );
+        }
+    }
+
+    /**
+     * @return void
+     */
+    private function dispatchEvent()
+    {
+        event( "view:{$this->name}", [ $this ] );
+    }
+
+    /**
+     * @return \Illuminate\Contracts\View\Factory|View
+     */
+    public function view()
+    {
+        return view( $this->name, $this->data );
+    }
+
+    /**
+     * @param $path
+     */
+    public function setPath( $path )
+    {
+        $this->name = $path;
+    }
+
+}
